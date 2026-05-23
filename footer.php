dcddac07579243ddee1e8f58a56e42a141a5e54c
michellive2021-lab <michellive2021@gmail.com>
2026-05-23 14:15:28 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+    <footer>
+        جميع الحقوق محفوظة &copy; <?php echo date('Y'); ?> - فني تركيب قفل ذكي في جميع أنحاء الرياض
+        <br>
+        <a href="tel:0555555555" style="color:#7ecbfa;">اتصل بنا الآن</a>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
